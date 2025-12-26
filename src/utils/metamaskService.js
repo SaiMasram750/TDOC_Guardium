02c99c33ee9d147b3762b11deddfc5ed637c81ce
SaiMasram750 <saimasram54@gmail.com>
2025-12-26 17:19:53 +0530

    Update metamaskService.js

--- src/utils/metamaskService.js
+++ src/utils/metamaskService.js
@@ -1,17 +1,3 @@
-
 export function isMetaMaskInstalled() {
   return typeof window.ethereum !== "undefined" && window.ethereum.isMetaMask;
-}
-
-export async function requestMetaMaskAccounts() {
-  if (!isMetaMaskInstalled()) {
-    throw new Error("MetaMask is not installed");
-  }
-  try {
-    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
-    return accounts;
-  } catch (err) {
-    console.error("MetaMask account request failed:", err);
-    throw err;
-  }
 }
